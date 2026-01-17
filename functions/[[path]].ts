956e5ba26aecf69d2876b2d5618d6998f41a3cd6
vuong20031591-hub <vuong20031591@gmail.com>
2026-01-17 16:30:11 +0700

    fix: correct server build path for Cloudflare Functions

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,7 +2,7 @@
 import { createPagesFunctionHandler } from "@react-router/cloudflare";
 
 // @ts-ignore - build output will exist at runtime
-import * as build from "../build/server/server.js";
+import * as build from "../build/server/index.js";
 
 export const onRequest = createPagesFunctionHandler({
   build,
